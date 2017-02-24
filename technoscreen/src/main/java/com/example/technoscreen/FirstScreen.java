4458f5d9022658f07aed8af4d9568750deb4df36
vikrammore246 <vikrammore246@gmail.com>
2017-02-24 16:40:23 +0530

    Codekul Youtube Screen

--- technoscreen/src/main/java/com/example/technoscreen/FirstScreen.java
+++ technoscreen/src/main/java/com/example/technoscreen/FirstScreen.java
@@ -1,11 +1,22 @@
 package com.example.technoscreen;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FirstScreen extends AppCompatActivity {
+
+    TextView text;
+
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
@@ -14,6 +25,7 @@ public class FirstScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+
 
         recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
 
